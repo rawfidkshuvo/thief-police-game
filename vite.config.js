51cdf9f0764547998701cd9ecba5722a8f00f274
rawfid <rawfid.shuvo@outlook.com>
2026-01-20 01:16:38 +0100

    tailwind v4 test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), tailwindcss(),
+  ],
   base: '/thief-police-game/', // MUST match your repo name
 })
